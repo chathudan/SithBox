9ec4b2f8210c25251416f486e8956252d32ab6d5
Chathura Wijesinghe <cdanasiri@gmail.com>
2014-11-16 00:22:59 +0530

    initial commit

---
+++ src/com/sithagi/sithbox/SithBoxActivity.java
@@ -0,0 +1,12 @@
+package com.sithagi.sithbox;
+
+import android.app.Activity;
+import android.os.Bundle; 
+public class SithBoxActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_sithbox);
+	}
+}
